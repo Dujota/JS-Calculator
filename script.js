a154f23a20c98b192a2533166f35ae4e456f4bcc
Dennis Budde <dennis@writerduet.com>
2023-06-15 18:40:28 -0500

    incorporated new learnings and broke out functions, added keyboard abilities

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const nums = Array.from(document.getElementsByClassName('number-button'));
-const ops = Array.from(document.getElementsByClassName('operation-button'));
 const equals = document.getElementById('=');
+const clear = document.getElementById('clear');
 const reset = document.getElementById('reset');
 const exp = document.getElementById('calc-display');
 const plus = document.getElementById('+');
@@ -15,65 +14,33 @@ let timeToClear = false;
 let opIs = "";
 let n1=0;
 
-// digit clicks 
-nums.forEach(num => {
-    num.addEventListener('click', function() {
-        const numText = this.innerText;
-        if (timeToClear) {
-            exp.innerText = "";
-            timeToClear = false;
-        }
-        exp.append(numText);
-    })
-})
-
-// Reset button resets on click, or operation if number field is already reset
-reset.addEventListener('click',function() {
-    if (exp.innerText == "") {
-        opIs = "";
-        plus.classList.remove("blue");
-        minus.classList.remove("blue");
-        multiply.classList.remove("blue");
-        divide.classList.remove("blue");
-    } else {
+// Functions 
+// number button click 
+function numClick(num) {
+    if (timeToClear) {
         exp.innerText = "";
+        timeToClear = false;
     }
-    timeToClear = false;
-})
+    exp.append(num);
+}
 
-// Equals button click handled separately because it only runs if everything 
-// is good for it to run; otherwise it doesn't do anything 
-equals.addEventListener('click',function() {
-    let expText = exp.innerText;
-    if (opIs && expText) {
+// operation click
+function opClick(expText,opText) {
+    if (!expText) {
+        return
+    }
+    if (!opIs) {
+        n1 = exp.innerText;
+    } else {
         n1 = runOp(Number(n1),Number(expText),opIs);
         exp.innerText = n1;
-        opIs="";
-        timeToClear = true;
     }
-})
+    opIs = opText;
+    // target.classList.add("blue");
+    timeToClear = true;
+}
 
-// Operation clicks - run the calc if it's ready and get the new operation ready.
-// otherwise they just get the operation ready and prep a new round of number entry. 
-ops.forEach(op => {
-    op.addEventListener('click',function() {
-        let expText = exp.innerText;
-        const opText = this.innerText;
-        if (!expText) {
-            return
-        }
-        if (!opIs) {
-            n1 = exp.innerText;
-        } else {
-            n1 = runOp(Number(n1),Number(expText),opIs);
-            exp.innerText = n1;
-        }
-        opIs = opText;
-        this.classList.add("blue");
-        timeToClear = true;
-    });
-})
-
+// function to run operations
 function runOp(n1,n2,op) {    
     switch(op) {
         case '+':
@@ -90,3 +57,75 @@ function runOp(n1,n2,op) {
             return n1/n2;
     }                
 }
+
+// function for clicking equals/return
+function equalsClick(expText) {
+    if (opIs && expText) {
+        n1 = runOp(Number(n1),Number(expText),opIs);
+        exp.innerText = n1;
+        opIs="";
+        timeToClear = true;
+    }
+}
+
+
+// Grab numbers and operations in bulk and then split out how they're handled by class
+// since other buttons are one-off, their event listener functions are separated out 
+frame.addEventListener('click', function(event) {
+    let target = event.target;
+    if (target.classList.contains("number-button")) {
+        const numText = target.innerText;
+        numClick(numText);
+    } else if (target.classList.contains("operation-button")) {
+        let expText = exp.innerText;
+        const opText = target.innerText;
+        opClick(expText,opText);
+    } 
+})
+
+
+// Keydowns 
+document.addEventListener('keydown',function(event) {
+    let target = event.key;
+    if (target > -1) {
+        numClick(target);
+    } else if (target=='+' || target=='-' || target=='/' || target=='*') {
+        let expText = exp.innerText;
+        opClick(expText,target);
+    } else if (target=='return') {
+        let expText = exp.innerText;
+        equalsClick(expText);
+    }
+})
+
+// Clear button clears on click, or operation if number field is already clear
+clear.addEventListener('click',function() {
+    if (exp.innerText == "") {
+        opIs = "";
+        plus.classList.remove("blue");
+        minus.classList.remove("blue");
+        multiply.classList.remove("blue");
+        divide.classList.remove("blue");
+    } else {
+        exp.innerText = "";
+    }
+    timeToClear = false;
+})
+
+// Reset all to default if Reset is clicked
+reset.addEventListener('click', function() {
+    exp.innerText="";
+    opIs="";
+    plus.classList.remove("blue");
+    minus.classList.remove("blue");
+    multiply.classList.remove("blue");
+    divide.classList.remove("blue");
+    timeToClear = false;
+})
+
+// Equals button click handled separately because it only runs if everything 
+// is good for it to run; otherwise it doesn't do anything 
+equals.addEventListener('click',function() {
+    let expText = exp.innerText;
+    equalsClick(expText); 
+})
