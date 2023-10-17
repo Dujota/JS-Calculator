0ee75e55944da6884105ed7671929d85e854d4fb
michellechoi126 <michellechoi126@gmail.com>
2023-10-16 17:36:44 -0700

    complete MVP

--- script.js
+++ script.js
@@ -4,31 +4,68 @@ const opBtnEl = document.querySelectorAll('.operation-button');
 const eqBtnEl = document.querySelector('.equals-button');
 const resetBtnEl = document.querySelector('.reset-button');
 
+let total = 0
+
 function displayNum(e) {
-    const text = e.target.id
-    display = h2El.innerText + text
-    h2El.innerText = display
+    if (total === 0) {
+        const text = e.target.id
+        display = h2El.innerText + text
+        h2El.innerText = display
+    } else if (h2El.classList.contains('clear')){
+        h2El.innerText = ''
+        const text = e.target.id
+        display = h2El.innerText + text
+        h2El.innerText = display
+        h2El.classList.remove('clear')
+    } else {
+        const text = e.target.id
+        display = h2El.innerText + text
+        h2El.innerText = display
+    }
 }
 
 numBtnEl.forEach(btn => {
     btn.addEventListener('click', displayNum)
 })
 
-function calculateNum(e) {
+function updateNum(e) {
     num = parseFloat(h2El.innerText)
     if (e.target.id === "/") {
-        console.log(e.target.id)
+        total = num
+        h2El.classList.add('/', 'clear')
     } else if (e.target.id === "*") {
-        console.log(e.target.id)
+        total = num
+        h2El.classList.add('*', 'clear')
     } else if (e.target.id === "-") {
-        console.log(e.target.id)
+        total = num
+        h2El.classList.add('-', 'clear')
     } else if (e.target.id === "+") {
-        console.log(e.target.id)
+        total = num
+        h2El.classList.add('+', 'clear')
+    } else {
+        console.log("valid operation button must be clicked")
     }
 }
 
 opBtnEl.forEach(btn => {
-    btn.addEventListener('click', calculateNum)
-    
+    btn.addEventListener('click', updateNum)
 })
 
+eqBtnEl.addEventListener('click', calcTotal)
+
+function calcTotal(e) {
+    num = parseFloat(h2El.innerText)
+    if (h2El.classList.value === '/') {
+        total /= num
+        h2El.innerText = total
+    } else if (h2El.classList.value === '*') {
+        total *= num
+        h2El.innerText = total
+    } else if (h2El.classList.value === '-') {
+        total -= num
+        h2El.innerText = total
+    } else if (h2El.classList.value === '+') {
+        total += num
+        h2El.innerText = total
+    }
+}
