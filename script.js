819b0e39e7eacf4d21a108758bad64a9d082f262
Victor <victor@whimsyniche.com>
2023-10-16 21:00:22 -0400

    While I'm still working on it. I just tidied up the JS

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 // UI
-
 let display = document.querySelector("calc-display");
 
 const keys = document.querySelectorAll(".number-button");
 
 let operators = document.querySelectorAll(".operation-button")
 
+// Event Handlers
 function operations(e) {
-    const whatThis = e.target.classList[0];
-    console.log(whatThis);
     display.innerHTML += parseInt(e.target.innerHTML);
 
 }
 //if a reset, clear or delete
 
 // Event Listeners
-
-console.log(keys);
 keys.forEach((e) => { e.addEventListener('click', operations)
 });
